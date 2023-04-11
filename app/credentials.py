090242fe9f5deb6389b0ea5ae676baeab6e5b322
s-swathib <105411200+s-swathib@users.noreply.github.com>
2023-04-11 12:35:12 +0530

    Update credentials.py

--- app/credentials.py
+++ app/credentials.py
@@ -1,18 +1,18 @@
 
 # We recommend leaving these two variables as below. The DATASOURCE_CONNECTION_STRING contains 9800 PDFs from Arxiv Computer Science publications. Change it if you want to use your own data.
 DATASOURCE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=depprocureformstorage;AccountKey=Ze0i+1gPZIw5NRNV/r9C+/comhFAX+jOCSNVzN7jSMwmQqCKf/Aw3oq/Qi3sUkLDLcFviEXNawbl+AStqOf7Hg==;EndpointSuffix=core.windows.net"
-DATASOURCE_SAS_TOKEN = "?sv=2021-12-02&ss=bfqt&srt=c&sp=rwdlacupiytfx&se=2023-04-07T20:22:16Z&st=2023-04-07T12:22:16Z&spr=https&sig=bjnMiEknAnF3b%2BINhD0waxZoRr3V1u26GNpGUtgZ1K4%3D"
+DATASOURCE_SAS_TOKEN = "?sv=2021-12-02&ss=bfqt&srt=c&sp=rwdlacupiytfx&se=2023-04-11T12:19:17Z&st=2023-04-11T04:19:17Z&spr=https&sig=258Vvyk7ItgxRr235yQNvoHhUuxHYv1cRFOrVYG4toI%3D"
 AZURE_SEARCH_API_VERSION = '2021-04-30-Preview'
 AZURE_OPENAI_API_VERSION = "2023-03-15-preview"
 
 # Change these below with your own services credentials
 
-AZURE_SEARCH_ENDPOINT = "https://azure-cog-search-cstevuxaqrxcm.search.windows.net"
-AZURE_SEARCH_KEY = "fET2lKwQ4akc6sFLF1YQ376eNRyrpJmspxSqMiB7eZAzSeBJZG43"
-COG_SERVICES_NAME = "cognitive-service-cstevuxaqrxcm"
-COG_SERVICES_KEY = "3cffb8c5ba1249df88399968a499ad54"
-AZURE_OPENAI_ENDPOINT = "https://open-ai-pinternal.openai.azure.com/"
-AZURE_OPENAI_KEY = "b2dc4d69945240a993288400d3b18faf"
+AZURE_SEARCH_ENDPOINT = "https://azure-cog-search-g2ysbgn5zpihq.search.windows.net"
+AZURE_SEARCH_KEY = "Uhv7jafKF7319Ms95bt3efeGE7e0FLGgR0eKltHvNcAzSeDXlfN5"
+COG_SERVICES_NAME = "cognitive-service-g2ysbgn5zpihq"
+COG_SERVICES_KEY = "4c76a8adf0ab431a8ecc1e78dcfa459d"
+AZURE_OPENAI_ENDPOINT = "https://openaipractice.openai.azure.com/"
+AZURE_OPENAI_KEY = "7b90a78f0e074117826c4a9f8205845e"
 
 # AZURE_SEARCH_ENDPOINT = "Enter your Azure Cognitive Search Endpoint ..."
 # AZURE_SEARCH_KEY = "Enter your Azure Cognitive Search Primary Key ..."
